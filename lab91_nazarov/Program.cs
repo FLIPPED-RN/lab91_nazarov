33b4fb21cf41d0b034bfa9a93e75209a64b3b8fa
Nazarov Ruslan <rnazarov142@gmail.com>
2023-12-09 14:37:59 +0200

    Добавьте файлы проекта.

---
+++ lab91_nazarov/Program.cs
@@ -0,0 +1,43 @@
+﻿try
+{
+    Console.Write("Введите первый катет: ");
+    double firstLeg1 = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    Console.Write("Введите второй катет: ");
+    double secondLeg1 = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    RightTriangle triangle1 = new RightTriangle(firstLeg1, secondLeg1);
+    Console.WriteLine(triangle1.GetInfo());
+    Console.WriteLine();
+    Console.Write($"Площадь: {triangle1.CalculateArea():F2}");
+    Console.WriteLine();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+class RightTriangle
+{
+    private double firstLeg;
+    private double secondLeg;
+
+    public RightTriangle(double firstLeg, double secondLeg)
+    {
+        this.firstLeg = firstLeg;
+        this.secondLeg = secondLeg;
+    }
+
+    public string GetInfo()
+    {
+        return $"Первый катет равен: {firstLeg}, Второй катет равен: {secondLeg}";
+
+    }
+
+    public double CalculateArea()
+    {
+        return 0.5 * firstLeg * secondLeg;
+    }
+}
